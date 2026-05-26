ad8e3fba4df0d98ee704fb0a1c7dbbfe981fe326
TalhaGillani186 <talhagillani186@gmail.com>
2026-05-26 15:42:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ df = pd.read_csv("car details v4.csv")
 # CLEAN DATA
 # -----------------------------
 
-df = df.dropna()
+# Remove rows where Price is missing
+df = df.dropna(subset=['Price'])
 
+# Convert Engine column safely
 df['Engine'] = df['Engine'].astype(str)
 
 df['Engine'] = df['Engine'].str.replace(' CC', '', regex=False)
@@ -32,7 +34,39 @@ df['Engine'] = pd.to_numeric(
 
 )
 
-df = df.dropna()
+# Fill missing engine values
+df['Engine'] = df['Engine'].fillna(
+
+    df['Engine'].median()
+
+)
+
+# Fill missing categorical values
+
+categorical_columns = [
+
+    'Make',
+    'Model',
+    'Fuel Type',
+    'Transmission',
+    'Drivetrain'
+
+]
+
+for col in categorical_columns:
+
+    df[col] = df[col].fillna(
+
+        df[col].mode()[0]
+
+    )
+
+# Fill missing Kilometer
+df['Kilometer'] = df['Kilometer'].fillna(
+
+    df['Kilometer'].median()
+
+)
 
 # -----------------------------
 # FEATURES & TARGET
